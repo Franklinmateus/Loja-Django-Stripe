9e538a2e24c59c4e1fd8cb308e2ac8ebe5565c05
Franklinmateus <frankmateus660@gmail.com>
2023-07-07 12:48:14 -0300

    adicionando slug de cada produto e cada tela

--- loja_django_stripe/loja/views.py
+++ loja_django_stripe/loja/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
-def product_details(request):
-    return render(request, 'loja/product_details.html')
+def product_details(request, slug):
+    product = get_object_or_404(Product, slug=slug)
+    return render(request, 'loja/product_details.html', {'product':product})
+
