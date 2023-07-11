e4f665d1d53126f41a2a9435916f168a7a3e2921
Franklinmateus <frankmateus660@gmail.com>
2023-07-11 09:07:07 -0300

    criando categorias especificas

--- loja_django_stripe/loja/views.py
+++ loja_django_stripe/loja/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Product
+from .models import Product, Category
 
-def product_details(request, slug):
+
+def category_details(request, slug):
+    category = get_object_or_404(Category, slug=slug)
+    products = category.products.all()
+    return render(request, 'loja/category_details.html', {'category' : category, 'products' : products})
+
+def product_details(request, category_slug, slug):
     product = get_object_or_404(Product, slug=slug)
-    return render(request, 'loja/product_details.html', {'product':product})
-
+    return render(request, 'loja/product_details.html', {'product' : product})
