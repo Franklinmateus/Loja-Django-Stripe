b63eb0795de899ea00ffee29cf7ae3c95133e864
Franklinmateus <frankmateus660@gmail.com>
2023-07-14 13:11:53 -0300

    adicionando imagens

--- loja_django_stripe/loja_django_stripe/urls.py
+++ loja_django_stripe/loja_django_stripe/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from core.views import frontpage, sobre
@@ -7,4 +9,4 @@ urlpatterns = [
     path('', include('loja.urls')),
     path('', frontpage, name='frontpage'),
     path('sobre', sobre, name='sobre'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
